b641b08f44f20d1fac2a9efec090e51f311dc792
Tandin Wangchen <taandinwangchen27@icloud.com>
2023-11-27 18:17:15 +0530

    fixing img break

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -27,11 +27,13 @@ const ProjectCard = ({
         className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
       >
         <div className='relative w-full h-[230px]'>
-          <img
-            src={image}
-            alt='project_image'
-            className='w-full h-full object-cover rounded-2xl'
-          />
+          {image && 
+              <img
+                src={image}
+                alt={name}
+                className="w-full h-full object-cover rounded-2xl"
+              />
+            }
 
           <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
             <div
